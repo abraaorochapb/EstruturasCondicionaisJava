df209fe9c2ec6b58ac5a0a2e3c86fd187e9a5683
abraaorochapb <abraaorochapb@gmail.com>
2023-08-14 14:08:50 -0300

    Mudança pelo padrão cumulativo

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,31 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Digite um número: ");
-        int x = sc.nextInt();
+        //System.out.print("Digite um número: ");
+        //int x = sc.nextInt();
 
-        if (x > 10) {
-            System.out.println(x + " é maior que 10");
-        } else if (x == 10) {
-            System.out.println(x + " é igual a 10");
-        } else {
-            System.out.println(x + " é menor que 10");
+        //if (x > 10) {
+        //    System.out.println(x + " é maior que 10");
+        //} else if (x == 10) {
+        //    System.out.println(x + " é igual a 10");
+        //} else {
+        //    System.out.println(x + " é menor que 10");
+        //}
+
+        System.out.print("Digite o número de minutos utilizados: ");
+        int minutos = sc.nextInt();
+
+        double fatura = 50.0;
+        if (minutos > 100) {
+            fatura += (minutos - 100) * 2;
+            System.out.printf("Sua fatura tem valor = R$ %.2f%n", fatura);
+        }
+        else {
+            System.out.printf("Sua fatura tem valor = R$ %.2f%n", fatura);
         }
 
         sc.close();
+
     }
 
 }
