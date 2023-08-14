a4c7c09bb453fc36b052dd9972e7792007b160f1
abraaorochapb <abraaorochapb@gmail.com>
2023-08-14 14:25:03 -0300

    descomentando o código

--- src/Main.java
+++ src/Main.java
@@ -4,54 +4,54 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        //System.out.print("Digite um número: ");
-        //int x = sc.nextInt();
+        System.out.print("Digite um número: ");
+        int x = sc.nextInt();
 
-        //if (x > 10) {
-        //    System.out.println(x + " é maior que 10");
-        //} else if (x == 10) {
-        //    System.out.println(x + " é igual a 10");
-        //} else {
-        //    System.out.println(x + " é menor que 10");
-        //}
+        if (x > 10) {
+            System.out.println(x + " é maior que 10");
+        } else if (x == 10) {
+            System.out.println(x + " é igual a 10");
+        } else {
+            System.out.println(x + " é menor que 10");
+        }
 
-        //System.out.print("Digite o número de minutos utilizados: ");
-        //int minutos = sc.nextInt();
+        System.out.print("Digite o número de minutos utilizados: ");
+        int minutos = sc.nextInt();
 
-        //double fatura = 50.0;
-        //if (minutos > 100) {
-        //    fatura += (minutos - 100) * 2;
-        //    System.out.printf("Sua fatura tem valor = R$ %.2f%n", fatura);
-        //}
-        //else {
-        //    System.out.printf("Sua fatura tem valor = R$ %.2f%n", fatura);
-        //}
+        double fatura = 50.0;
+        if (minutos > 100) {
+            fatura += (minutos - 100) * 2;
+            System.out.printf("Sua fatura tem valor = R$ %.2f%n", fatura);
+        }
+        else {
+            System.out.printf("Sua fatura tem valor = R$ %.2f%n", fatura);
+        }
 
-        //System.out.print("Digite o numero do dia na semana: ");
-        //int dia = sc.nextInt();
+        System.out.print("Digite o numero do dia na semana: ");
+        int dia = sc.nextInt();
 
-        //switch (dia) {
-        //    case 1:
-        //        System.out.println("Domingo");
-        //        break;
-        //    case 2:
-        //        System.out.println("Segunda");
-        //        break;
-        //    case 3:
-        //        System.out.println("Terça");
-        //        break;
-        //    case 4:
-        //        System.out.println("Quarta");
-        //        break;
-        //    case 5:
-        //        System.out.println("Quinta");
-        //        break;
-        //    case 6:
-        //        System.out.println("Sexta");
-        //        break;
-        //    case 7:
-        //       System.out.println("Sábado");
-        //}
+        switch (dia) {
+            case 1:
+                System.out.println("Domingo");
+                break;
+            case 2:
+                System.out.println("Segunda");
+                break;
+            case 3:
+                System.out.println("Terça");
+                break;
+            case 4:
+                System.out.println("Quarta");
+                break;
+            case 5:
+                System.out.println("Quinta");
+                break;
+            case 6:
+                System.out.println("Sexta");
+                      break;
+            case 7:
+               System.out.println("Sábado");
+        }
 
 
         double preco = 34.0;
